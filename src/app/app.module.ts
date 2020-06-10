1b0b47ce5c33ec77931bcabbbd7027208325ce47
yalayas <yal@yal.spb.ru>
2020-06-10 17:29:33 +0300

    Компоненты 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import * as $ from 'jquery';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { TransferComponent } from './transfer/transfer.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { ContactsComponent } from './contacts/contacts.component';
+
+
+// определение маршрутов
+const appRoutes: Routes = [
+  {path: 'reservation', component: ReservationComponent},
+  {path: 'contacts', component: ContactsComponent},
+  {path: 'reviews', component: ReviewsComponent},
+  {path: 'transfer', component: TransferComponent},
+  {path: 'about', component: AboutComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    ReservationComponent,
+    TransferComponent,
+    ReviewsComponent,
+    ContactsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
