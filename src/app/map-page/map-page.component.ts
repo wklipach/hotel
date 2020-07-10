3f78470de3fa23faa71e2b40c025ebba3603652d
klipach <klipach@mail.ru>
2020-07-10 19:52:42 +0300

    54321

--- src/app/map-page/map-page.component.ts
+++ src/app/map-page/map-page.component.ts
@@ -43,12 +43,13 @@ export class MapPageComponent implements OnInit {
 
   renderMap() {
 
+    // tslint:disable-next-line: no-string-literal
     window['initMap'] = () => {
       this.loadMap();
-    }
+    };
 
-    if(!window.document.getElementById('google-map-script')) {
-      var s = window.document.createElement('script');
+    if (!window.document.getElementById('google-map-script')) {
+      const s = window.document.createElement('script');
       s.id = 'google-map-script';
       s.type = 'text/javascript';
       s.src =  'https://maps.googleapis.com/maps/api/js?key=AIzaSyA74254mNXG7sVoYX-qpgwWYlXV7jB_IO4&callback=initMap';
@@ -60,7 +61,7 @@ export class MapPageComponent implements OnInit {
   }
 
   loadMap = () => {
-    var map = new window['google'].maps.Map(this.mapElement.nativeElement, {
+    const map = new window.google.maps.Map(this.mapElement.nativeElement, {
       center: {lat: 59.903043, lng: 29.076565},
       zoom: 17
     });
@@ -68,21 +69,21 @@ export class MapPageComponent implements OnInit {
     // tslint:disable-next-line: forin
     for (const indLoc in this.locations) {
       const location = this.locations[indLoc];
-      const marker = new window['google'].maps.Marker({
-        position: {lat: location['lat'], lng: location['lng']},
-        map: map,
-        title: location['title'],
+      const marker = new window.google.maps.Marker({
+        position: {lat: location.lat, lng: location.lng},
+        map,
+        title: location.title,
         draggable: true,
-        animation: window['google'].maps.Animation.DROP,
+        animation: window.google.maps.Animation.DROP,
       });
 
-      const strContentString = this.contentString(indLoc, location['data1'], location['data2']);
+      const strContentString = this.contentString(indLoc, location.data1, location.data2);
 
-      const infowindow = new window['google'].maps.InfoWindow({
+      const infowindow = new window.google.maps.InfoWindow({
         content: strContentString
       });
 
-        marker.addListener('click', function() {
+      marker.addListener('click', () => {
           infowindow.open(map, marker);
         });
 
@@ -116,18 +117,18 @@ export class MapPageComponent implements OnInit {
     '<p>"+79117448813"</p>'+
     '</div>'+
     '</div>';
-   
+
     var infowindow = new window['google'].maps.InfoWindow({
       content: contentString
     });
-   
+
       marker.addListener('click', function() {
         infowindow.open(map, marker);
       });
 
-*/      
+*/
 
-/*      
+/*
       let latLng = {lat: Number(location.latitude), lng: Number(location.longitude)};
       let marker = new google.maps.Marker({
         position: latLng,
@@ -144,7 +145,7 @@ export class MapPageComponent implements OnInit {
   return '<div id="content' + id + '">' +
          '<div id="siteNotice' + id + '"></div>' +
          '<h3 id="thirdHeading' + id + '" class="thirdHeading">' + data1 + '</h3>' +
-         '<div id="bodyContent' + id + '">'+
+         '<div id="bodyContent' + id + '">' +
          '<p>"' + data2 + '"</p>' +
          '</div>' +
          '</div>';
