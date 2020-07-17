a5de92648f6865c7212ee3717d5b1741cb0fecad
klipach <klipach@mail.ru>
2020-07-17 16:46:26 +0300

    добавил в About-Room

--- src/app/about-room/about-room.component.ts
+++ src/app/about-room/about-room.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { EasycalendarComponent } from '../components/easycalendar/easycalendar.component';
 
 import {NgxGalleryOptions} from '@kolkov/ngx-gallery';
 import {NgxGalleryImage} from '@kolkov/ngx-gallery';
@@ -13,6 +14,15 @@ export class AboutRoomComponent implements OnInit {
 
   galleryOptions: NgxGalleryOptions[];
   galleryImages: NgxGalleryImage[];
+
+  @ViewChild('beginCalendar') beginCalendar: EasycalendarComponent;
+  @ViewChild('endCalendar') endCalendar: EasycalendarComponent;
+
+  boolBeginCalendar = false;
+  boolEndCalendar = false;
+  numberOfAdults = 1;
+  numberOfChildren = 0;
+
 
   constructor() { }
 
@@ -96,5 +106,58 @@ export class AboutRoomComponent implements OnInit {
     ];
 
   }
+
+  clickIconBeginCalendar() {
+    this.boolBeginCalendar = !this.boolBeginCalendar;
+    console.log('this.boolBeginCalendar', this.boolBeginCalendar);
+  }
+
+  clickIconEndCalendar() {
+    this.boolEndCalendar = !this.boolEndCalendar;
+  }
+
+
+  clickDayBegin(strDate) {
+    this.beginCalendar.clear();
+    document.getElementById('textCalenarBegin'). textContent = strDate;
+    this.boolBeginCalendar = false;
+
+  }
+
+  clickDayEnd(strDate) {
+    this.endCalendar.clear();
+    document.getElementById('textCalenarEnd'). textContent = strDate;
+    this.boolEndCalendar = false;
+
+  }
+
+  addAdult() {
+    this.numberOfAdults = this.numberOfAdults + 1;
+  }
+
+  deleteAdult() {
+
+    if (this.numberOfAdults > 1) {
+    this.numberOfAdults = this.numberOfAdults - 1;
+    }
+
+  }
+
+
+  addChildren() {
+    this.numberOfChildren = this.numberOfChildren + 1;
+  }
+
+  deleteChildren() {
+
+    if (this.numberOfChildren > 0) {
+        this.numberOfChildren = this.numberOfChildren - 1;
+    }
+  }
+
+  findRooms() {
+    console.log('найти комнаты');
+  }
+
 
 }
