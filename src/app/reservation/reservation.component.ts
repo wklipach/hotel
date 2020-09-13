356635fa5bd792a94ae7ce1bca4cdc999b556565
klipach <klipach@mail.ru>
2020-09-13 14:00:40 +0300

    room size

--- src/app/reservation/reservation.component.ts
+++ src/app/reservation/reservation.component.ts
@@ -203,7 +203,6 @@ export class ReservationComponent implements OnInit {
     return new Date(parts[2], parts[1] - 1, parts[0]);
   }
 
-
   isValidDate(d) {
     return d instanceof Date && !isNaN( d.getTime());
   }
